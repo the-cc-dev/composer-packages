db23d43b95f3e91e06363757dd3217467aca8dd6
the-cc-dev <the.cc.dev@gmail.com>
2019-09-03 01:06:27 -0500

    init

--- pug-filter-twig/.history/src/Pug/Filter/Base_20190903003241.php
+++ pug-filter-twig/.history/src/Pug/Filter/Base_20190903003241.php
@@ -10,10 +10,6 @@ class Base extends AbstractFilter
 {
     public function __invoke(Filter $node, Compiler $compiler)
     {
-        foreach ($node->block->nodes as $linne) {
-            $output[] = $compiler->interpolate($line->value);
-        }
-
         return $this->getNodeString($node, $compiler);
     }
 }
--- pug-filter-twig/.history/src/Pug/Filter/Base_20190903010005.php
+++ pug-filter-twig/.history/src/Pug/Filter/Base_20190903010005.php
@@ -10,9 +10,7 @@ class Base extends AbstractFilter
 {
     public function __invoke(Filter $node, Compiler $compiler)
     {
-        foreach ($node->block->nodes as $linne) {
-            $output[] = $compiler->interpolate($line->value);
-        }
+
 
         return $this->getNodeString($node, $compiler);
     }
